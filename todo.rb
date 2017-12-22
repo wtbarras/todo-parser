9ff9e12ab1779917f2aae3c2ec4ee2cd780b350c
nemo <William T Barras>
2017-12-22 12:00:19 -0700

    Add comments and strip useless line prefix

--- todo.rb
+++ todo.rb
@@ -13,12 +13,18 @@ for i in 0..input_length-1
   file_array[i] = File.open(input_array[i])
 end
 
-# Print out file contents
+# Go through each file that was specified
 file_array.each do |file|
+  # Print file name
   puts file.path
   @line_number = 1
+  # Go through each line in the file
   file.readlines.each do |line|
+    # Check if the line includes the target text
     if line.include? "@TODO"
+      # Strip comment and todo off front of line
+      line = line[line.index("@TODO") + 6, line.length - 1]
+      # Print line number and message
       printf "%d: %s\n", @line_number, line
     end
     @line_number += 1
