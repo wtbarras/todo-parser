4c538bd029c7ab212d6261280ec78ec20257d789
nemo <William T Barras>
2017-12-22 09:15:27 -0700

    Add functionality to open all specified files and then print out lines with @TODO in them

--- todo.rb
+++ todo.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/ruby
 
+# @TODO Add support for unix style arguments e.g. -c <config files>-f <files>
+
+# Get command line arguments
 input_array = ARGV
-for i in 0..input_array.length
-  file = File.open(input_array[0])
+input_length = input_array.length
+# Make array to hold file object that will be parsed
+file_array = Array.new(input_length)
+
+# Open file objects on specified files
+for i in 0..input_length-1
+  file_array[i] = File.open(input_array[i])
+end
+
+# Print out file contents
+file_array.each do |file|
+  puts file.path
+  file.readlines.each do |line|
+    if line.include? "@TODO"
+      puts line
+    end
+  end
 end
